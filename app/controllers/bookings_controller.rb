fb6d7fd184aac1c676273e2517f230145ab2641d
cyril Marek <marek.cyril@gmail.com>
2023-11-21 16:24:31 +0100

    booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,28 @@
 class BookingsController < ApplicationController
-    def show
+  def show
+    @actor = Actor.find(params[:actor_id])
+    @booking = Booking.find(@actor)
+  end
 
+  def new
+    @actor = Actor.find(params[:actor_id])
+    @booking = Booking.new
+  end
 
+  def create
+    @booking = Booking.new(booking_params)
+    @actor = Actor.find(params[:actor_id])
+    @bookmark.actor = @actor
+    if @bookmark.save
+      redirect_to actor_path(@actor)
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def new
-      @actor = Actor.find(params[:actor_id])
-      @booking = Booking.new
-    end
+  private
 
-    def create
-    end
-
-
-    private
-
-    def booking_params
-      params.require(:booking).permit(:actor_id,)
-    end
-
+  def booking_params
+    params.require(:booking).permit(:actor_id, :start_date, :end_date, :type)
+  end
 end
