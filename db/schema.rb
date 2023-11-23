4b88f98804d63a739b9ecdd4252e3e5c415b469b
cyril Marek <marek.cyril@gmail.com>
2023-11-23 16:43:53 +0100

    categories after final fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_111536) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_151744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_111536) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "actor_categories", force: :cascade do |t|
+    t.bigint "actor_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["actor_id"], name: "index_actor_categories_on_actor_id"
+    t.index ["category_id"], name: "index_actor_categories_on_category_id"
   end
 
   create_table "actors", force: :cascade do |t|
@@ -91,6 +100,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_111536) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "actor_categories", "actors"
+  add_foreign_key "actor_categories", "categories"
   add_foreign_key "actors", "users"
   add_foreign_key "bookings", "actors"
   add_foreign_key "bookings", "users"
