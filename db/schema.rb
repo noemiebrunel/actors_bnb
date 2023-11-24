da6ea175ab4eceb85c00630c943e042e27627763
juan desitter <yuri.golovanov@protonmail.com>
2023-11-24 12:23:39 +0100

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -77,6 +77,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_160427) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "dashboards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
